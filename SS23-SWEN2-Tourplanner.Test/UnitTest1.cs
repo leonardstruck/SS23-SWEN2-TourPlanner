668d75b39acc59aa75363ea5d19cd2135b3ac2e3
Leonard Struck <me@leonardstruck.com>
2023-03-16 17:14:16 +0100

    init Test project

---
+++ SS23-SWEN2-Tourplanner.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace SS23_SWEN2_Tourplanner.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
