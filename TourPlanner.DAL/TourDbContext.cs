920488b7719bb10528522919b320093d9baf8699
Bernhard Utz <if21b241@technikum-wien.at>
2023-04-12 11:35:23 +0200

    add docker compose

---
+++ TourPlanner.DAL/TourDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TourPlanner.DAL
+{
+	/*
+	public class TourDbContext : DbContext
+	{
+		public TourDbContext()
+		{
+		}
+		
+		// todo import DbContext
+		// protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		// {
+		// 	// todo change connection string
+		// 	optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TourPlanner;Trusted_Connection=True;");
+		// }
+	}
+	*/
+}
+
